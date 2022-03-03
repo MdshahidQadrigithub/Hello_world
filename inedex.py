caaed88973444f383d764690e6e3720f2aaad270
Md Shahid Qadri <100861080+MdshahidQadrigithub@users.noreply.github.com>
2022-03-03 22:10:34 +0530

    Add files via upload

---
+++ inedex.py
@@ -0,0 +1,16 @@
+a = "innoovatum"  # global variable
+
+
+def abd():
+    a = "Awesome."  # local variable
+    print("My career is a " + a)# output for local variable
+
+
+
+abd()
+
+print('Engineering ' + a) # output for global variable
+
+
+x=5
+print(type(5))
